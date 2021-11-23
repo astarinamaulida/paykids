65bfa90962f1cd90201a9a8b12585fe803ce1f60
Nick <nsagias@gmail.com>
2021-11-23 15:56:33 -0500

    update boilder pplate router location

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+
 
 const app = express();
 
@@ -13,6 +12,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
