a0a35b739aa0c27c23a485659120279f66505f60
Nick <nsagias@gmail.com>
2021-11-25 18:18:06 -0500

    Create creditcard component with useStates

---
+++ frontend/cardComponentsTemp/CreditCard.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+
+export default function CreditCard(props) {
+ 
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [bank, setBank] = useState('');
+  const [creditCardCompany, setCreditCardCompany] = useState('')
+  const [cardNumber, setCardNumber] = useState('');
+  const [expirationMonth, setExpirationMonth] = useState(10);
+  const [expirationYear, setExpirationYear] = useState(22);
+  const [CVV, setCVV] = useState(111);
+
+  const [sitePreferences, setsitePreferences] = useState({})
+  return (
+    <></>
+  )
+}
+ 
