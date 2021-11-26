ec9c29b4ec50a2f9fd5e07a213738c8e84a78c92
Nick <nsagias@gmail.com>
2021-11-26 17:54:33 -0500

    Add money request endpoint

--- backend/routes/requests.js
+++ backend/routes/requests.js
@@ -6,4 +6,12 @@ requests.get('/', function(req, res, next) {
   res.json({});
 });
 
+requests.get('/request_money', function(req, res, next) {
+  res.json({});
+});
+
+requests.get('/request_money/:request_money_id', function(req, res, next) {
+  res.json({});
+});
+
 module.exports = requests;
