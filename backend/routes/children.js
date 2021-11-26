74d29c81f1b394b3c863d217d6566ea90cb4537a
Nick <nsagias@gmail.com>
2021-11-26 17:46:16 -0500

    add children enpoints

--- backend/routes/children.js
+++ backend/routes/children.js
@@ -6,4 +6,19 @@ children.get('/', function(req, res, next) {
   res.json({});
 });
 
+children.get('/:child_id', function(req, res, next) {
+  res.json({});
+});
+children.get('/:child_id/balance', function(req, res, next) {
+  res.json({});
+});
+
+children.get('/:child_id/transactions', function(req, res, next) {
+  res.json({});
+});
+
+children.get('/:child_id/transactions/:transaction_id', function(req, res, next) {
+  res.json({});
+});
+
 module.exports = children;
