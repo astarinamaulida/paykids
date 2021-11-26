d04f2e9c7bf15c122c16357ab0af9c47f5a7cf8a
Nick <nsagias@gmail.com>
2021-11-26 14:15:53 -0500

    create childrens route

--- backend/routes/children.js
+++ backend/routes/children.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const router = express.Router();
+const children = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+children.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-module.exports = router;
+module.exports = children;
