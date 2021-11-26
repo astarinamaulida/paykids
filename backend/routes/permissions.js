e4e207a3d905a8d76f94a76551a35b135ff7f425
Nick <nsagias@gmail.com>
2021-11-26 18:07:45 -0500

    Add permissions

--- backend/routes/permissions.js
+++ backend/routes/permissions.js
@@ -6,4 +6,12 @@ permissions.get('/', function(req, res, next) {
   res.json({});
 });
 
+permissions.get('/account_status/:child_id', function(req, res, next) {
+  res.json({});
+});
+
+permissions.get('/account_status/:child_id/:permission_id', function(req, res, next) {
+  res.json({});
+});
+
 module.exports = permissions;
