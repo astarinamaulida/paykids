16a783481dfe71fe546a917bf7f789cc36acc4cc
Nick <nsagias@gmail.com>
2021-11-27 13:19:33 -0500

    add request list item

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import ChildrensTransactionList from './components/ChildrensTransactionList';
+import ChildrensRequestList from './components/ChildrensRequestList';
 
 
 function App() {
   return (
     <div className="App">
       <h1>Transaction List</h1>
-      <ChildrensTransactionList />
+      {/* <ChildrensTransactionList /> */}
+      <ChildrensRequestList />
     </div>
   );
 }
--- frontend/src/components/ChildrensRequestList.jsx
+++ frontend/src/components/ChildrensRequestList.jsx
@@ -1,9 +1,15 @@
-
+import ChildrensTransactionListItem from "./ChildrensTransactionListItem";
 
 export default function ChildrensRequestList(props) {
   return (
     <div>
       <h1>I am ChildrensRequestList</h1>
+      <ChildrensTransactionListItem />
+      <ChildrensTransactionListItem />
+      <ChildrensTransactionListItem />
+      <ChildrensTransactionListItem />
+      <ChildrensTransactionListItem />
+      <ChildrensTransactionListItem />
     </div>
   )
 }
