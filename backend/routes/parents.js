2f393ea37a44cbe7fb33b1c08c25046b739e836b
Nick <nsagias@gmail.com>
2021-11-27 14:03:27 -0500

    Add post money route for transactions

--- backend/routes/parents.js
+++ backend/routes/parents.js
@@ -28,4 +28,18 @@ parents.get('/parents/:parents_id/request_money/:child_id', function(req, res, n
 });
 
 
+// SendMoneyMoney {
+//   id: number;
+//   child_id: string;
+//   parent_id: string
+//   send_money_note: string;   // should be $5, $10, $15, $20, $50, $100 
+//   send_amount: number
+//   created_at: datetime [default: `now()`];
+// }
+
+parents.post('/parents/:parents_id/send_money/:child_id', function(req, res, next) {
+  res.json({});
+});
+
+
 module.exports = parents;
