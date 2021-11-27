1d586f674e3947b32272b664a0600707ab137020
Nick <nsagias@gmail.com>
2021-11-27 14:18:03 -0500

    add transaction notes

--- backend/routes/children.js
+++ backend/routes/children.js
@@ -6,15 +6,24 @@ children.get('/', function(req, res, next) {
   res.json({});
 });
 
+
 children.get('/:child_id', function(req, res, next) {
+
   res.json({});
 });
+
+
+// interface child_balance {
+//   id: number;
+//   account_balance: number;
+// }
 children.get('/:child_id/balance', function(req, res, next) {
+
   res.json({});
 });
 
 
-// recentTransactions {
+// interface recentTransactions {
 //   id: number;
 //   child_id: string;
 //   account_number: string;
@@ -35,9 +44,28 @@ children.get('/:child_id/transactions', function(req, res, next) {
 });
 
 children.get('/:child_id/transactions/:transaction_id', function(req, res, next) {
-  res.json({});
+
+  const transactions = [
+    { id: 1 , child_id: 1, account_number: 1, transaction_amount: 50, transaction_note: 'Saphora'},
+    { id: 2 , child_id: 2, account_number: 2, transaction_amount: 52, transaction_note: 'Game'},
+    { id: 3 , child_id: 3, account_number: 3, transaction_amount: 53, transaction_note: 'Saphora'}
+  ];
+  const transaction = transactions[transaction_id];
+  res.json({transaction});
 });
 
+
+// /api/children/:child_id/:balance_id
+// interface childAccount {
+//   id: number;
+//   Child_id: string;
+//   first_name: string;
+//   last_name: string;
+//   Account_number: string;
+//   account_balance: number;
+//   Account_cvv: number;
+//   expiry_date: Date;
+// }
 children.get('/children/:child_id/account_data/:account_id', function(req, res, next) {
   res.json({});
 });
