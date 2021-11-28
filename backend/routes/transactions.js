a3b77881e0d4da58fa8b9b485371b7f9b7f561f8
Nick <nsagias@gmail.com>
2021-11-28 10:58:55 -0500

    update all names to be requests from transactions

--- backend/routes/transactions.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const transactions = express.Router();
-
-// Access all transactions
-transactions.get('/', function(req, res, next) {
-  res.json({});
-});
-
-module.exports = transactions;
