1195fd6ed119d11a8e7d4ddbe6ebf345bc671765
Astarina Maulida <astarina.maulida@gmail.com>
2021-11-28 18:42:03 -0500

    Add components and views

---
+++ component_temp1/src/views/JaneSendMoney.js
@@ -0,0 +1,55 @@
+import React from "react";
+import Button from "muicss/lib/react/button";
+
+function JaneSendMoney() {
+  return (
+    <main className="container">
+      {/* <article className="frame-send-money">
+        <div className="text">
+          <p>Send Money: Tina</p>
+        </div>
+        <div>
+			    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/muicss/0.9.41/css/mui.min.css" />
+           <div>
+            <Button className="button-money">$5</Button>
+            <Button className="button-money">$10</Button>
+            <Button className="button-money">$15</Button>
+          </div>
+          <div>
+            <Button className="button-money">$20</Button>
+            <Button className="button-money">$50</Button>
+            <Button className="button-money2">$100</Button>
+          </div>
+        </div>
+        <p>Amount to give: $15</p>
+        <div>
+          <Button className="button-confirm">Confirm</Button>
+          <Button className="button-cancel">Cancel</Button>
+        </div>
+      </article>
+      <article className="frame">
+        <span>
+        <p>Recent Transactions</p>
+        </span>
+          <div className="row">
+            <div className="column-left">www.sephora.com</div>
+            <div className="column-right">$200</div>
+          </div>
+          <div>
+            <div className="column-left">www.sephora.com</div>
+            <div className="column-right">$200</div>
+          </div>
+          <div>
+            <div className="column-left">www.sephora.com</div>
+            <div className="column-right">$200</div>
+          </div>
+          <div>
+            <div className="column-left">www.sephora.com</div>
+            <div className="column-right">$200</div>
+          </div>
+      </article> */}
+    </main>
+  );
+}
+
+export default JaneSendMoney;
---
+++ component_temp1/src/views/TinaPicksCard.js
@@ -0,0 +1,43 @@
+import React from "react"; //optional
+import Button from "muicss/lib/react/button";
+
+function TinaPicksCard() {
+  const cc_main_design = "https://i.ibb.co/bRxw8RD/kids-1.png";
+  const cc_design2 = "https://i.ibb.co/BVLD4x7/cc-design2.png";
+  const cc_design3 = "https://i.ibb.co/9HZTs4B/cc-design3.png";
+  const cc_design4 = "https://i.ibb.co/16G1vHQ/cc-design4.png";
+  const cc_design5 = "https://i.ibb.co/8DrWzNB/cc-design5.png";
+  const cc_design6 = "https://i.ibb.co/tD9LbWb/cc-design6.png";
+
+  return (
+    <main className="container">
+      {/* <article className="frame-pick-cards">
+      <div>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/muicss/0.9.41/css/mui.min.css" />
+          <ul class="gallery">
+            <li class="gallery-item">
+              <img className="cc-main-design" src={cc_main_design}/>
+            </li>
+            <li class="gallery-item">
+              <img className="cc-design2" src={cc_design2}/>
+            </li>
+            <li class="gallery-item">
+              <img className="cc-design3" src={cc_design3}/>
+            </li>
+            <li class="gallery-item">
+              <img className="cc-design4" src={cc_design4}/>
+            </li>
+            <li class="gallery-item">
+              <img className="cc-design5" src={cc_design5}/>
+            </li>
+            <li class="gallery-item">
+              <img className="cc-design6" src={cc_design6}/>
+            </li>
+          </ul>
+       </div>
+      </article> */}
+    </main>
+  );
+}
+
+export default TinaPicksCard;
