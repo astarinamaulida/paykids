60f443ce99c8bac7c89e0a70ebecfd0c90097251
Nick <nsagias@gmail.com>
2021-11-28 22:49:26 -0500

    Create childrens list under parents

---
+++ frontend/src/components/ParentsChidlrenList.jsx
@@ -0,0 +1,17 @@
+import ParentsApprovalListItem from "./ParentsApprovaIListItem";
+
+export default function ParentsApprovalList(props) {
+  return (
+    <div>
+      <h1>I am ParentsApprovalList</h1>
+      <section>
+        <ParentsApprovalListItem />
+        <ParentsApprovalListItem />
+        <ParentsApprovalListItem />
+        <ParentsApprovalListItem />
+        <ParentsApprovalListItem />
+        <ParentsApprovalListItem />
+        </section>
+    </div>
+  )
+}
