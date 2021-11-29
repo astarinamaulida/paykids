52cf63450ceb092b8a50f7b2ce17db57926ca5dd
Astarina Maulida <astarina.maulida@gmail.com>
2021-11-29 15:29:51 -0500

    Add backup and combine buttons and interfaces

--- frontend/src/components/ChildrensVisualSettings.jsx
+++ frontend/src/components/ChildrensVisualSettings.jsx
@@ -1,15 +1,21 @@
 import { useState } from "react";
-import { Button } from "@mui/material";
-// import Button from 'muicss/lib/react/button';
+import Button from 'muicss/lib/react/button';
 
 export default function ChildrensVisualSettings(props) {
-  const [cardBackground, setCardBackground] = useState("ccMainDesign");
+  const [cardBackground, setCardBackground] = useState("https://i.ibb.co/bRxw8RD/kids-1.png");
+
+  const cc_main_design = "https://i.ibb.co/bRxw8RD/kids-1.png";
+  const cc_design2 = "https://i.ibb.co/BVLD4x7/cc-design2.png";
+  const cc_design3 = "https://i.ibb.co/9HZTs4B/cc-design3.png";
+  const cc_design4 = "https://i.ibb.co/16G1vHQ/cc-design4.png";
+  const cc_design5 = "https://i.ibb.co/8DrWzNB/cc-design5.png";
+  const cc_design6 = "https://i.ibb.co/tD9LbWb/cc-design6.png";
 
   const CC_DESIGN_2 = "https://i.ibb.co/BVLD4x7/cc-design2.png";
   const CC_DESIGN_3 = "https://i.ibb.co/9HZTs4B/cc-design3.png";
   const CC_DESIGN_4 = "https://i.ibb.co/16G1vHQ/cc-design4.png";
-  const CC_DESIGN_5 = "https://i.ibb.co/16G1vHQ/cc-design5.png";
-  const CC_DESIGN_6 = "https://i.ibb.co/16G1vHQ/cc-design6.png";
+  const CC_DESIGN_5 = "https://i.ibb.co/8DrWzNB/cc-design5.png";
+  const CC_DESIGN_6 = "https://i.ibb.co/tD9LbWb/cc-design6.png";
   const CC_DESIGN_MAIN = "https://i.ibb.co/bRxw8RD/kids-1.png";
 
   const ccDesignTwo = function () {
@@ -32,51 +38,38 @@ export default function ChildrensVisualSettings(props) {
   };
 
   return (
-    <div className="container">
+    <main className="container">
       <div className="frame-card-selection">
-        <h1>I am ChildrensVisualSettings</h1>
-        <link
+      <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/muicss/0.9.41/css/mui.min.css"
         />
-        <Button onClick={ccMainDesign} className="cc-main-design">
-          Default
-        </Button>
-        <div class="gallery-item">
-          <Button onClick={ccDesignTwo} className="cc-design2">
-            White
-          </Button>
+          <div class="gallery-item">
+            <img className="cc-main-design" src={cardBackground} />
+          </div>
+          <div class="gallery-item">
+            <img onClick={ccMainDesign} className="cc-main-design" src={cc_main_design} />
+          </div>
+          <div class="gallery-item">
+            <img onClick={ccDesignTwo}  className="cc-design2" src={cc_design2} />
+          </div>
+          <div class="gallery-item">
+            <img onClick={ccDesignThree} className="cc-design3" src={cc_design3} />
+          </div>
+          <div class="gallery-item">
+            <img onClick={ccDesignFour} className="cc-design4" src={cc_design4} />
+          </div>
+          <div class="gallery-item">
+            <img onClick={ccDesignFive} className="cc-design5" src={cc_design5} />
+          </div>
+          <div class="gallery-item">
+            <img onClick={ccDesignSix} className="cc-design6" src={cc_design6} />
+          </div>
+          <div>
+          <Button className="button-select">Select Card</Button>
         </div>
-        <div class="gallery-item">
-          <Button onClick={ccDesignThree} className="cc-design3">
-            Pink
-          </Button>
-        </div>
-        <div class="gallery-item">
-          <Button onClick={ccDesignFour} className="cc-design4">
-            Red
-          </Button>
-        </div>
-        <div class="gallery-item">
-          <Button onClick={ccDesignFive} className="cc-design5">
-            green
-          </Button>
-        </div>
-        <div class="gallery-item">
-          <Button onClick={ccDesignSix} className="cc-design6">
-            Orange
-          </Button>
-        </div>
-        <div class="gallery-item">
-          <Button onClick={ccMainDesign} className="cc-main-design">
-            Default
-          </Button>
-        </div>
-        {/* this is a form need event prvent default */}
-        <div>
-        <Button className="button-select">Confirm</Button>
-        </div>
+        
       </div>
-    </div>
+    </main>
   );
 }
