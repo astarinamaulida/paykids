3e59872492f774df40075c934a45a31e36228545
Nick <nsagias@gmail.com>
2021-11-28 21:23:53 -0500

    prepare helper functions to set state for background design

--- frontend/src/components/ChildrensVisualSettings.jsx
+++ frontend/src/components/ChildrensVisualSettings.jsx
@@ -1,4 +1,37 @@
+import { useState } from "react"
+import Button from 'muicss/lib/react/button';
+
 export default function ChildrensVisualSettings(props) {
+  const [cardBackground, setCardBackground] = useState("ccMainDesign");
+  
+  const CC_DESIGN_2 = "";
+  const CC_DESIGN_3 = "";
+  const CC_DESIGN_4 = "";
+  const CC_DESIGN_5 = "";
+  const CC_DESIGN_6 = "";
+  const CC_DESIGN_MAIN = "";
+
+  const ccDesignTwo = function() {
+    setCardBackground(CC_DESIGN_2)
+  }
+  const ccDesignThree = function() {
+    setCardBackground(CC_DESIGN_3)
+  }
+  const ccDesignFour = function() {
+    setCardBackground(CC_DESIGN_4)
+  }
+  const ccDesignFive = function() {
+    setCardBackground(CC_DESIGN_5)
+  }
+  const ccDesignSix = function() {
+    setCardBackground(CC_DESIGN_6 )
+  }
+  const ccMainDesign = function() {
+    setCardBackground(CC_DESIGN_MAIN)
+  }
+ 
+  
+  
   return (
     <div>
       <h1>I am ChildrensVisualSettings</h1>
