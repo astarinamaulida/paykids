f252217dbe9a57a091fc6f69e79dd4a65d94c1c1
Nick <nsagias@gmail.com>
2021-11-28 22:57:20 -0500

    add local data

--- frontend/src/components/ParentsChidlrenList.jsx
+++ frontend/src/components/ParentsChidlrenList.jsx
@@ -1,6 +1,48 @@
 import ParentsChildrenListItem from "./ParentsChildrenListItem";
 
 export default function ParentsChildrenList(props) {
+  let childrensList = [
+    { 
+      id: 1 , 
+      child_id: 1, 
+      parent_id: 1,
+      first_name: "Tina", 
+      last_name: "Toaster",  
+      account_number: 1, 
+      account_balance: 500,
+      active: true
+    },
+    { 
+      id: 2 , 
+      child_id: 2,
+      parent_id: 1, 
+      first_name: "Bob", 
+      last_name: "Toaster",  
+      account_number: 2, 
+      account_balance: 200,
+      active: true
+    }
+  ];
+  
+  
+  let parentsList = [
+    { 
+      id: 1 , 
+      child_id: 1, 
+      first_name: "Jane", 
+      last_name: "Toaster",  
+      account_number: 1, 
+      account_balance: 500,
+      active: true,
+      childrensList: childrensList 
+    }
+  ]
+
+
+  // const parsedTransactions = Array.isArray(transactionsList) && 
+  //   transactionsList.map(transaction => (
+  //     <ParentsChildrenListItem   {...transaction} key={transaction.id} />));
+  
   return (
     <div>
       <h1>I am ParentsChildrenlList</h1>
