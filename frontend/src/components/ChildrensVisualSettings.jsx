3bbcf829d4d96f85d0c9ae148fe083cbe1d5904e
Astarina Maulida <astarina.maulida@gmail.com>
2021-11-29 15:38:00 -0500

    Refactor card as button

--- frontend/src/components/ChildrensVisualSettings.jsx
+++ frontend/src/components/ChildrensVisualSettings.jsx
@@ -2,21 +2,16 @@ import { useState } from "react";
 import Button from 'muicss/lib/react/button';
 
 export default function ChildrensVisualSettings(props) {
-  const [cardBackground, setCardBackground] = useState("https://i.ibb.co/bRxw8RD/kids-1.png");
+  const CC_DESIGN_MAIN = "https://i.ibb.co/bRxw8RD/kids-1.png";
 
-  const cc_main_design = "https://i.ibb.co/bRxw8RD/kids-1.png";
-  const cc_design2 = "https://i.ibb.co/BVLD4x7/cc-design2.png";
-  const cc_design3 = "https://i.ibb.co/9HZTs4B/cc-design3.png";
-  const cc_design4 = "https://i.ibb.co/16G1vHQ/cc-design4.png";
-  const cc_design5 = "https://i.ibb.co/8DrWzNB/cc-design5.png";
-  const cc_design6 = "https://i.ibb.co/tD9LbWb/cc-design6.png";
-
+  const [cardBackground, setCardBackground] = useState(CC_DESIGN_MAIN);
+  
   const CC_DESIGN_2 = "https://i.ibb.co/BVLD4x7/cc-design2.png";
   const CC_DESIGN_3 = "https://i.ibb.co/9HZTs4B/cc-design3.png";
   const CC_DESIGN_4 = "https://i.ibb.co/16G1vHQ/cc-design4.png";
   const CC_DESIGN_5 = "https://i.ibb.co/8DrWzNB/cc-design5.png";
   const CC_DESIGN_6 = "https://i.ibb.co/tD9LbWb/cc-design6.png";
-  const CC_DESIGN_MAIN = "https://i.ibb.co/bRxw8RD/kids-1.png";
+
 
   const ccDesignTwo = function () {
     setCardBackground(CC_DESIGN_2);
@@ -48,22 +43,22 @@ export default function ChildrensVisualSettings(props) {
             <img className="cc-main-design" src={cardBackground} />
           </div>
           <div class="gallery-item">
-            <img onClick={ccMainDesign} className="cc-main-design" src={cc_main_design} />
+            <img onClick={ccMainDesign} className="cc-main-design" src={CC_DESIGN_MAIN} />
           </div>
           <div class="gallery-item">
-            <img onClick={ccDesignTwo}  className="cc-design2" src={cc_design2} />
+            <img onClick={ccDesignTwo}  className="cc-design2" src={CC_DESIGN_2} />
           </div>
           <div class="gallery-item">
-            <img onClick={ccDesignThree} className="cc-design3" src={cc_design3} />
+            <img onClick={ccDesignThree} className="cc-design3" src={CC_DESIGN_3} />
           </div>
           <div class="gallery-item">
-            <img onClick={ccDesignFour} className="cc-design4" src={cc_design4} />
+            <img onClick={ccDesignFour} className="cc-design4" src={CC_DESIGN_4} />
           </div>
           <div class="gallery-item">
-            <img onClick={ccDesignFive} className="cc-design5" src={cc_design5} />
+            <img onClick={ccDesignFive} className="cc-design5" src={CC_DESIGN_5} />
           </div>
           <div class="gallery-item">
-            <img onClick={ccDesignSix} className="cc-design6" src={cc_design6} />
+            <img onClick={ccDesignSix} className="cc-design6" src={CC_DESIGN_6} />
           </div>
           <div>
           <Button className="button-select">Select Card</Button>
