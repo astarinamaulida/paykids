3fde3693c612f33aec244791b79dc0afb937ec99
Nick <nsagias@gmail.com>
2021-11-28 20:55:14 -0500

    Create sendmoney state

--- frontend/src/components/ParentsSendMoneyForm.jsx
+++ frontend/src/components/ParentsSendMoneyForm.jsx
@@ -1,4 +1,30 @@
+import { useState } from "react"
+
 export default function ParentsSendMoneyForm(props) {
+  const [sendMoney, setSendMoney] = useState(0);
+
+  const five = function() {
+    setSendMoney(sendMoney = 5)
+  }
+  const ten = function() {
+    setSendMoney(sendMoney = 10)
+  }
+  const fifteen = function() {
+    setSendMoney(sendMoney = 15)
+  }
+  const twenty = function() {
+    setSendMoney(sendMoney = 20)
+  }
+  const fifty = function() {
+    setSendMoney(sendMoney = 50)
+  }
+  const hundred = function() {
+    setSendMoney(sendMoney = 100)
+  }
+  
+
+
+
   return (
     <div>
       <h1>I am ParentsSendMoneyForm</h1>
