649541c5d6631eeae2623467fbbc9bace0e0b3b4
Nick <nsagias@gmail.com>
2021-11-28 22:27:54 -0500

    Add dynamic data

--- frontend/src/components/ChildrensTransactionListItem.jsx
+++ frontend/src/components/ChildrensTransactionListItem.jsx
@@ -1,9 +1,10 @@
 export default function ChildrensTransactionListItem(props) {
+  const {transaction_amount, transaction_note} = props;
+
   return (
     <div>
-      <section>
-        <h3>ChildrensTransactionListItem</h3>
-      </section>
+        <h3>{transaction_note}</h3>
+        <h3>{transaction_amount}</h3>
     </div>
   )
 }
