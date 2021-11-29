f3f26e7cf76ccfbd90c71e5816571a478da10726
Nick <nsagias@gmail.com>
2021-11-28 21:10:03 -0500

    Add stylesheet but not implemented

--- frontend/src/components/ParentsSendMoneyForm.jsx
+++ frontend/src/components/ParentsSendMoneyForm.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react"
 import Button from 'muicss/lib/react/button';
 
+// import "/components/Button.scss"
+
 export default function ParentsSendMoneyForm(props) {
   const [sendMoney, setSendMoney] = useState(0);
 
