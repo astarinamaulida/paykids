0837126f95bf7f4adcac37f44905049a34a34ba7
Astarina Maulida <astarina.maulida@gmail.com>
2021-11-30 06:21:27 -0500

    Add styling to component

--- frontend/src/components/ChildrensRequestList.jsx
+++ frontend/src/components/ChildrensRequestList.jsx
@@ -5,7 +5,7 @@ export default function ChildrensRequestList(props) {
   const requestsList = [
     { id: 1 , child_id: 1, account_number: 1, request_amount: 5,  request_note: 'lunch money'},
     { id: 2 , child_id: 1, account_number: 1, request_amount: 15, request_note: 'hair thing'},
-    { id: 3 , child_id: 1, account_number: 1, request_amount: 30, request_note: 'Saphora'}
+    { id: 3 , child_id: 1, account_number: 1, request_amount: 30, request_note: 'Sephora'}
   ];
   
   const parsedRequests = Array.isArray(requestsList) && 
