651e9a4e597502032cbe543be1ff1c7e378b12ac
Astarina Maulida <astarina.maulida@gmail.com>
2022-01-01 19:14:49 -0500

    Refactoring

--- frontend/src/components/LandingPageBackground.jsx
+++ frontend/src/components/LandingPageBackground.jsx
@@ -5,9 +5,13 @@ export default function LandingPageBackground(props) {
   return (
     <div>
       <div className="landingpage"></div>
-      <Button  className="button-kids-login"> <a href="/childrenslogin">Kids Login</a></Button>
-      <Button  className="button-parents-login"><a href="/childrenslogin">Parents Login</a></Button>
-      </div>
-      
+      <Button className="button-kids-login">
+        {" "}
+        <a href="/childrenslogin">Kids Login</a>
+      </Button>
+      <Button className="button-parents-login">
+        <a href="/childrenslogin">Parents Login</a>
+      </Button>
+    </div>
   );
 }
